99ff195fa473e0baf83d5aa4a2141381c1bd7c26
Agustin Soria <soriaagustin8@gmail.com>
2023-09-13 04:19:48 -0300

    Update index BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
@@ -10,10 +10,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Routes>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
