06f0713450cd932cc5ce87486bad889313d2e448
Agustin Soria <soriaagustin8@gmail.com>
2023-09-13 04:23:08 -0300

    Dispatch cards in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchHeroes } from './redux/Heroes/heroesSlice';
 import Heroes from './components/Heroes';
 import CardHeroes from './components/CardHeroes';
-import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchHeroes());
+  }, [dispatch]);
   return (
     <>
       <Routes>
         <Route path="/" element={<Heroes />} />
-        <Route path="/cardsheroes" element={<CardHeroes />} />
+        <Route path="/heroes/:heroeId" element={<CardHeroes />} />
       </Routes>
     </>
   );
